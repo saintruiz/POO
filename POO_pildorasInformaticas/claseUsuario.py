e1f3668ede8d95c186530b20522f08d76576406f
saintruiz <saruizt@unal.edu.co>
2023-12-15 19:26:36 -0500

    Agregue una nueva carpeta con lo que estoy viendo en POO con el curso

---
+++ POO_pildorasInformaticas/claseUsuario.py
@@ -0,0 +1,30 @@
+class usuario():
+    def __init__(self, userName, userAge, userResidance):
+
+        self.userName=userName
+        self.userAge=userAge
+        self.userResidance=userResidance
+
+    def desciption(self):
+       description=(f"Nombre: {self.userName}, Edad: {self.userAge}, Lugar de residencia: {self.userResidance}") 
+       return description
+    
+class empleado(usuario):
+    def __init__(self, salario, antiguedad, nombreEmpleado, edadEmpleado, residenciaEmpleado):
+        super().__init__(nombreEmpleado, edadEmpleado, residenciaEmpleado)
+        self.salario=salario
+        self.antiguedad=antiguedad
+
+
+    def desciption(self):
+        print(super().desciption())
+        descripcionEmpleado= (f"Salario del empleado: {self.salario}, Antiguedad del empleado: {self.antiguedad}")
+        return descripcionEmpleado
+    
+
+userSantiago=usuario('Manuel', 55, 'Colombia')
+print(userSantiago.desciption())
+
+print(isinstance(userSantiago, empleado))
+
+
